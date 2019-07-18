f569922dce58d1d3e6fd47f260e7471db80e087a
SethWickham <seth.wickham@hotmail.com>
2019-07-18 13:20:04 -0700

    added Pointers

--- Pointers.go
+++ Pointers.go
@@ -5,5 +5,12 @@ import (
 )
 
 func pointers() {
-	fmt.Println("pointers")
+	fmt.Println("Pointers Print START:")
+
+	x := 542
+	fmt.Println("Value of x:", x, "Address of x:", x)
+	b := &x
+	fmt.Println("Value of b:", b)
+	fmt.Printf("Type of b: %T\n ", b)
+	fmt.Println("Pointers Print END")
 }
