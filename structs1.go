d63ecec5711be370799e605ae73b60e91f04621f
SethWickham <seth.wickham@hotmail.com>
2019-07-26 14:36:12 -0700

    fixed comments on structs1.go

--- structs1.go
+++ structs1.go
@@ -44,7 +44,7 @@ func structs1() {
 	fmt.Println("The Elephant's Name is :", zNames.elephantName)
 	fmt.Println("The Toucan's Name is :", zNames.toucanName)
 	// in Go our inner TYPE gets promoted to the outer TYPE so when we want to call tigerDanger we don't need to specify
-	// that it is of TYPE zooAnimals, instead we can just call our TYPE from the outer TYPE zooNorCal.
+	// that it is of TYPE zooAnimalNames, instead we can just call our outer TYPE.
 	fmt.Println("is", zNames.tigerName, "dangerous?", ZooNorCal.tigerDanger)
 
 	fmt.Println("structs1 Print END")
