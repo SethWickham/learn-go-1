6503a1ae00619c1328606fdcb46a6e1f0237aee1
SethWickham <seth.wickham@hotmail.com>
2019-07-26 14:32:45 -0700

    added comments onto structs1.go file

--- structs1.go
+++ structs1.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 )
 
+//Here we are creating two structs in go
 type zooAnimalNames struct {
 	elephantName string
 	toucanName   string
 	tigerName    string
 }
 
+//  here we are embedding our zooAnimalNames struct
+// into in our dangerousZooAnimals struct so that the Values can be accessed later on
 type dangerousZooAnimals struct {
 	zooAnimalNames
 	elephantDanger bool
@@ -25,6 +28,7 @@ func structs1() {
 			toucanName:   "Tory",
 			tigerName:    "George",
 		},
+
 		elephantDanger: false,
 		toucanDanger:   false,
 		tigerDanger:    true,
@@ -39,7 +43,8 @@ func structs1() {
 	fmt.Println("structs1 Print START")
 	fmt.Println("The Elephant's Name is :", zNames.elephantName)
 	fmt.Println("The Toucan's Name is :", zNames.toucanName)
-
+	// in Go our inner TYPE gets promoted to the outer TYPE so when we want to call tigerDanger we don't need to specify
+	// that it is of TYPE zooAnimals, instead we can just call our TYPE from the outer TYPE zooNorCal.
 	fmt.Println("is", zNames.tigerName, "dangerous?", ZooNorCal.tigerDanger)
 
 	fmt.Println("structs1 Print END")
